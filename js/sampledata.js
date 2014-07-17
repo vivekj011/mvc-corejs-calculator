3f932dfb7cf662aac78fe74d3d05f1235002fdbc
vivek jagtap <vivekj011@gmail.com>
2014-07-17 14:14:27 +0530

    Create sampledata.js
    sample data to populate buttons, display in calculator

---
+++ js/sampledata.js
@@ -0,0 +1,77 @@
+var calculatorAttr = {
+	type : "Normal"
+}
+
+var display = {
+	type : "LED"
+};
+
+var buttons = [ {
+	type : "operand",
+	actualValue : 7,
+	displayValue : "7"
+}, {
+	type : "operand",
+	actualValue : 8,
+	displayValue : "8"
+}, {
+	type : "operand",
+	actualValue : 9,
+	displayValue : "9"
+}, {
+	type : "operator",
+	actualValue : "/",
+	displayValue : "/"
+}, {
+	type : "operand",
+	actualValue : 4,
+	displayValue : "4"
+}, {
+	type : "operand",
+	actualValue : 5,
+	displayValue : "5"
+}, {
+	type : "operand",
+	actualValue : 6,
+	displayValue : "6"
+}, {
+	type : "operator",
+	actualValue : "X",
+	displayValue : "X"
+}, {
+	type : "operand",
+	actualValue : 1,
+	displayValue : "1"
+}, {
+	type : "operand",
+	actualValue : 2,
+	displayValue : "2"
+}, {
+	type : "operand",
+	actualValue : 3,
+	displayValue : "3"
+}, {
+	type : "operator",
+	actualValue : "-",
+	displayValue : "-"
+}, {
+	type : "decimal",
+	actualValue : ".",
+	displayValue : "."
+}, {
+	type : "operand",
+	actualValue : 0,
+	displayValue : "0"
+}, {
+	type : "operator",
+	actualValue : "calculate",
+	displayValue : "="
+}, {
+	type : "operator",
+	actualValue : "+",
+	displayValue : "+"
+},{
+	type : "clear",
+	actualValue : "clear",
+	displayValue : "C"
+} ];
