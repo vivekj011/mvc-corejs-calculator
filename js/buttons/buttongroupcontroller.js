a63e5af53a4337e4f96207b492ab1796df71c420
vivek jagtap <vivekj011@gmail.com>
2014-07-17 14:08:18 +0530

    Create buttongroupcontroller.js
    to create all the buttons

---
+++ js/buttons/buttongroupcontroller.js
@@ -0,0 +1,33 @@
+function ButtonGroupController(options){
+	this.collection = new package.CALC.buttons.collection(options.buttons);
+
+	var buttons = [];
+	var buttonsColl = this.collection.getAll();
+	for (var i =0; i < buttonsColl.length; i++) {
+		var buttonModel = buttonsColl[i];
+		var button = new package.CALC.buttons.button.controller({
+																	el : options.el,//button-container
+																	buttonModel : buttonModel																	
+																});
+
+		buttons.push(button);
+	}
+
+	this.buttonGroupView = new package.CALC.buttons.buttonGroupView({
+																		callback : this.buttonClicked.bind(this)
+																	});
+
+}
+
+ButtonGroupController.prototype = new package.CALC.parentClass();
+ButtonGroupController.prototype.constructor = ButtonGroupController;
+
+ButtonGroupController.prototype.buttonClicked = function(options) {
+	debugger;
+	var buttonData = this.collection.get(options);
+	this.fireEvent("buttonClicked",buttonData.attributes);
+};
+
+
+
+package.CALC.buttons.buttonGroupController = ButtonGroupController;
