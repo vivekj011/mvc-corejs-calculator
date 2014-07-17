ddc2b2b872230a60bad8690846f94f4268212ce6
vivek jagtap <vivekj011@gmail.com>
2014-07-17 15:50:20 +0530

    Update calcmodel.js
    added methods to query on state and to calculate.

--- js/calculator/calcmodel.js
+++ js/calculator/calcmodel.js
@@ -11,7 +11,6 @@ function CalcModel(options){
 
 	this.initialize(options);
 }
-
 CalcModel.prototype.initialize = function(options) {
 	if (typeof (options) !== 'undefined') {
 		var attributes = this.attributes;
@@ -25,6 +24,100 @@ CalcModel.prototype.initialize = function(options) {
 		}
 	}
 }
+
+CalcModel.prototype.isLeftOperandDefined = function() {
+	return (this.attributes.firstOperand !== undefined);
+};
+
+CalcModel.prototype.setDecimalToRightOperand = function() {
+	this.attributes.secondOperand = this.attributes.secondOperand.toFixed(1);
+};
+
+CalcModel.prototype.setDecimalToLeftOperand = function() {
+	this.attributes.firstOperand = this.attributes.firstOperand.toFixed(1);
+};
+
+CalcModel.prototype.isRightOperandDefined = function() {
+	return (this.attributes.secondOperand !== undefined);
+};
+
+CalcModel.prototype.isOperatorDefined = function() {
+	return (this.attributes.operator !== undefined);
+};
+
+CalcModel.prototype.concatenateToRightOperand = function(numberToConcatenate) {
+	var number = (this.attributes.secondOperand ? this.attributes.secondOperand +'' + numberToConcatenate : numberToConcatenate);
+	this.attributes.secondOperand = new Number(number).valueOf();
+};
+
+CalcModel.prototype.concatenateToLeftOperand = function(numberToConcatenate) {
+	var number = (this.attributes.firstOperand ? this.attributes.firstOperand +'' + numberToConcatenate : numberToConcatenate);
+	this.attributes.firstOperand = new Number(number).valueOf();
+};
+
+CalcModel.prototype.setLeftOperand = function(number) {
+	if(number){
+		this.attributes.firstOperand = new Number(number).valueOf();
+	}else{
+		throw new Error("invalid number to set");
+	}
+};
+
+CalcModel.prototype.calculate = function() {
+	var operator = this.getOperator();
+	var leftOperand = this.getLeftOperand();
+	var rightOperand = this.getRightOperand();
+
+	if(operator === "+"){
+		return (leftOperand + rightOperand);
+	}else if(operator === "-"){
+		return (leftOperand - rightOperand);
+	}else if(operator === "/"){
+		return (leftOperand / rightOperand);
+	}else if(operator === "X"){
+		return (leftOperand * rightOperand);
+	}else{
+		throw new Error("invalid state to calculate");
+	}
+};
+
+CalcModel.prototype.getLeftOperand = function() {
+	return this.attributes.firstOperand;//return copy / clone
+};
+
+
+CalcModel.prototype.getRightOperand = function() {
+	return this.attributes.secondOperand;//return copy / clone
+};
+
+
+CalcModel.prototype.getOperator = function() {
+	return this.attributes.operator;
+};
+
+CalcModel.prototype.setRightOperand = function(number) {
+	if(number){
+		this.attributes.secondOperand = new Number(number).valueOf();	
+	}else{
+		throw new Error("invalid number to set");
+	}
+};
+
+
+CalcModel.prototype.setOperator = function(operator) {
+	if('+'===operator || '-'===operator || 'X' ===operator || '/' === operator){
+		this.attributes.operator = operator;	
+	}else{
+		throw new Error("operator not supported");
+	}
+};
+
+CalcModel.prototype.resetOperandsAndOperator = function() {
+	this.attributes.firstOperand = undefined;
+	this.attributes.secondOperand = undefined;
+	this.attributes.operator = undefined;
+	this.attributes.result = undefined;
+};
 
 CalcModel.prototype.checkType = function(key, value) {
 	//it will check the type of value and accordingly decide that provided button property is valid or not 
